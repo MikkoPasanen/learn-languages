7c516ff04478b5c0b3af033e70894a73efcacb8d
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-13 18:58:05 +0200

    ../routes/languages.js: Add new router for post requests

--- backend/routes/languages.js
+++ backend/routes/languages.js
@@ -5,10 +5,18 @@ const languageRouter = express.Router();
 // Handles requests for fetching all results from a db table
 languageRouter.get('/:lang', async (req, res) => {
   try {
-    const language = req.params.lang;
-    const results = await database.fetchAll(language);
+    const results = await database.fetchAll(req.params.lang);
 
     res.json(results);
+  } catch (err) {
+    res.status(err.status).json(err.msg);
+  }
+});
+
+languageRouter.post('/:lang', async (req, res) => {
+  try {
+    const newData = await database.addData(req.params.lang, req.body);
+    res.status(201).json(newData);
   } catch (err) {
     res.status(err.status).json(err.msg);
   }
