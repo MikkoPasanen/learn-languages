7e8bf5cd99a4055fd63b7e9b1ebc5198fedac72b
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-17 00:44:17 +0200

    frontend/: Try out if fetching data from backends API work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {createTheme, ThemeProvider} from "@mui/material/styles"
 import { CssBaseline } from "@mui/material";
 import Box from "@mui/material/Box"
@@ -7,6 +7,18 @@ import TopAppBar from"./components/TopAppBar.jsx"
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    fetchExercises();
+  }, []);
+
+  const fetchExercises = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/home`);
+    const data = await response.json();
+    console.log(data);
+    setExercises(data);
+  };
 
   const theme = createTheme({
     palette: {
@@ -20,6 +32,9 @@ export default function App() {
         <CssBaseline />
         <Box sx={{ flexGrow: 1 }}>
           <TopAppBar darkMode={darkMode} handleThemeChange={() => setDarkMode(!darkMode)}/>
+          {exercises.map((exercise) => (
+            <div key={exercise.id}>{exercise.name}</div>
+          ))}
         </Box>
       </ThemeProvider>
     </>
