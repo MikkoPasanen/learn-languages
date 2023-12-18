4111aae0752dd291bd398fd1997061ac3bb027a9
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-18 15:26:38 +0200

    frontend/: Assing link to sign up page to a button in TopAppBar

--- frontend/src/components/TopAppBar.jsx
+++ frontend/src/components/TopAppBar.jsx
@@ -3,11 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
 
 import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
@@ -41,13 +41,31 @@ export default function TopAppBar({darkMode, handleThemeChange}) {
               </Typography>
             </Link>
           </IconButton>
-          <Box>
-            <Button
-              color="inherit"
-              sx={{ mr: 1 }}
+          <Box sx={{display: 'flex'}}>
+            <Link
+              to="/signin"
+              style={{
+                textDecoration: 'none',
+                color: 'inherit',
+                display: 'flex',
+                alignItems: 'center',
+              }}
             >
-              Login
-            </Button>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              >
+                <LoginIcon sx={{ pr: 1, fontSize: '2rem' }} />
+                <Typography
+                  variant="h6"
+                  component="div"
+                >
+                  Sign In
+                </Typography>
+              </IconButton>
+            </Link>
             <IconButton
               size="large"
               edge="end"
