4ddb992341f4c8da870ff24784312da237a3ee8c
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-18 14:48:29 +0200

    frontend/: Add basic card layout for exercises on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable react/prop-types */
+import { Box, Card, CardHeader, CardContent, Typography, IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 export default function Home({exercises}) {
     return (
-        <>
-
-        </>
-    )
+      <>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+          {exercises.map((exercise) => (
+            <Card key={exercise.id} sx={{mt: 3}}>
+              <CardHeader
+                action={
+                  <IconButton aria-label="settings" sx={{ml: 2}}>
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={exercise.name}
+                subheader="Test"
+              />
+              <CardContent>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  Moro
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+      </>
+    );
 }
