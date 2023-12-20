19976b45df4a452f583a5d49da6799594d8df299
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-20 20:29:34 +0200

    frontend/: Move exercise card rendering into their own component for clarity

---
+++ frontend/src/components/ExerciseCard.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable react/prop-types */
+import { Card, Box, CardHeader, CardContent, Typography, IconButton, Chip, Menu, MenuItem} from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { useState} from 'react';
+
+export default function ExerciseCard({ exerciseName, exerciseCategory, exerciseLanguage, exerciseId }) {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+   const handleOptionsClick = (e) => {
+     setAnchorEl(e.currentTarget);
+   };
+
+   const handleOptionsClose = () => {
+     setAnchorEl(null);
+   };
+
+    return (
+      <>
+        <Card
+          key={exerciseId}
+          sx={{ mt: 3, borderRadius: 4 }}
+        >
+          <CardHeader
+            action={
+              <IconButton
+                aria-label="settings"
+                sx={{ ml: 2 }}
+                onClick={handleOptionsClick}
+              >
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={exerciseName}
+            subheader={
+              <Box sx={{ mt: 1 }}>
+                <Chip
+                  sx={{ mr: 1 }}
+                  label={exerciseCategory}
+                />
+                <Chip label={exerciseLanguage} />
+              </Box>
+            }
+          ></CardHeader>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleOptionsClose}
+          >
+            <MenuItem onClick={handleOptionsClose}>
+              <IconButton sx={{ padding: 0 }}>
+                <EditIcon sx={{ fontSize: 15 }} />
+                <Typography sx={{ ml: 1, fontSize: 15 }}>Edit</Typography>
+              </IconButton>
+            </MenuItem>
+
+            <MenuItem onClick={handleOptionsClose}>
+              <IconButton sx={{ padding: 0 }}>
+                <DeleteIcon sx={{ fontSize: 15, color: '#d7094f' }} />
+                <Typography sx={{ ml: 1, fontSize: 15, color: '#d7094f' }}>
+                  Delete
+                </Typography>
+              </IconButton>
+            </MenuItem>
+          </Menu>
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+            >
+              Moro
+            </Typography>
+          </CardContent>
+        </Card>
+      </>
+    );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/prop-types */
-import { Box, Card, CardHeader, CardContent, Typography, IconButton, Chip, Menu, MenuItem} from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Box } from '@mui/material';
 import { useState, useEffect } from 'react';
+
+import ExerciseCard from '../components/ExerciseCard';
 
 export default function Home() {
     const [exercises, setExercises] = useState([]);
-    const [anchorEl, setAnchorEl] = useState(null);
 
     useEffect(() => {
       fetchExercises();
@@ -17,14 +15,6 @@ export default function Home() {
       const response = await fetch(`${import.meta.env.VITE_API_URL}/api/home`);
       const data = await response.json();
       setExercises(data);
-    };
-
-    const handleOptionsClick = (e) => {
-        setAnchorEl(e.currentTarget);
-    };
-
-    const handleOptionsClose = () => {
-        setAnchorEl(null);
     };
 
     return (
@@ -37,61 +27,13 @@ export default function Home() {
           }}
         >
           {exercises.map((exercise) => (
-            <Card
+            <ExerciseCard
               key={exercise.id}
-              sx={{ mt: 3, borderRadius: 4 }}
-            >
-              <CardHeader
-                action={
-                  <IconButton
-                    aria-label="settings"
-                    sx={{ ml: 2 }}
-                    onClick={handleOptionsClick}
-                  >
-                    <MoreVertIcon />
-                  </IconButton>
-                }
-                title={exercise.name}
-                subheader={
-                  <Box sx={{ mt: 1 }}>
-                    <Chip
-                      sx={{ mr: 1 }}
-                      label={exercise.category}
-                    />
-                    <Chip label={exercise.language} />
-                  </Box>
-                }
-              ></CardHeader>
-              <Menu
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={handleOptionsClose}
-              >
-                <MenuItem onClick={handleOptionsClose}>
-                  <IconButton sx={{ padding: 0 }}>
-                    <EditIcon sx={{ fontSize: 15 }} />
-                    <Typography sx={{ ml: 1, fontSize: 15 }}>Edit</Typography>
-                  </IconButton>
-                </MenuItem>
-
-                <MenuItem onClick={handleOptionsClose}>
-                  <IconButton sx={{ padding: 0 }}>
-                    <DeleteIcon sx={{ fontSize: 15, color: '#d7094f' }} />
-                    <Typography sx={{ ml: 1, fontSize: 15, color: '#d7094f' }}>
-                      Delete
-                    </Typography>
-                  </IconButton>
-                </MenuItem>
-              </Menu>
-              <CardContent>
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                >
-                  Moro
-                </Typography>
-              </CardContent>
-            </Card>
+              exerciseId={exercise.id}
+              exerciseName={exercise.name}
+              exerciseCategory={exercise.category}
+              exerciseLanguage={exercise.language}
+            />
           ))}
         </Box>
       </>
