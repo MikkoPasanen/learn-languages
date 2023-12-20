c09c17e386a25bd8aac0a858bf88b3d4886cd78d
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-20 21:41:50 +0200

    frontend/: Add exercise question game to exercise page

--- frontend/src/pages/Exercise.jsx
+++ frontend/src/pages/Exercise.jsx
@@ -1,28 +1,61 @@
 import { Box, Typography, TextField, Button } from "@mui/material"
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-export default function Exercise({ id }) {
+export default function Exercise() {
+    const { id } = useParams();
     const [wordPairs, setWordPairs] = useState([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(null);
     const [score, setScore] = useState(0);
     const [answer, setAnswer] = useState("");
 
     useEffect(() => {
-        fetchWordPairs();
-    });
+        fetchWordPairs(id);
+    }, [id]);
 
-    const fetchWordPairs = async () => {
+    const fetchWordPairs = async (id) => {
         const hr = await fetch(`${import.meta.env.VITE_API_URL}/api/exercise/${id}`);
         const data = await hr.json();
         setWordPairs(data);
+        console.log(data);
     };
 
+    const handlePlay = () => {
+        setCurrentQuestionIndex(0);
+    };
+
+    const handleNextQuestion = () => {
+        if (answer === wordPairs[currentQuestionIndex].finnish_word) {
+            setScore(score + 1);
+        }
+
+        setAnswer("");
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+    };
+
+    if (currentQuestionIndex === null) {
+        return (
+            <Button onClick={handlePlay}>Start studying</Button>
+        );
+    }
+
+    if (currentQuestionIndex === wordPairs.length) {
+        return (
+            <>
+                <Typography variant="h3">Your score: {score}/{wordPairs.length}</Typography>
+            </>
+        );
+    }
 
     return (
-        <>
-        <Box>
-            <Typography variant="h3">Exercise</Typography>
-        </Box>
-        </>
+     <Box>
+            <Typography variant="h3">Translate the word:</Typography>
+            <Typography variant="h5">{wordPairs[currentQuestionIndex].foreign_word}</Typography>
+            <TextField
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+            />
+            <Button onClick={handleNextQuestion}>Next</Button>
+     </Box>
     )
 }
