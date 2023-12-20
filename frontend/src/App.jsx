70e6efd523e3966b4a7e4a8f7544e973ea54eda0
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-20 20:50:53 +0200

    frontend/: Add router for exercise page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from "./pages/Home.jsx";
+import Exercise from "./pages/Exercise.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import TopAppBar from"./components/TopAppBar.jsx";
 import SignIn from "./pages/SignIn.jsx";
@@ -26,6 +27,7 @@ export default function App() {
             <TopAppBar darkMode={darkMode} handleThemeChange={() => setDarkMode(!darkMode)}/>
               <Routes>
                 <Route path="/" element={<Home/>} />
+                <Route path="/exercise/:id" element={<Exercise/>} />
                 <Route path="/signin" element={<SignIn/>} />
                 <Route path="/signup" element={<SignUp/>} />
                 <Route path="*" element={<ErrorPage/>} />
