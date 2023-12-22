9b7faabb1ae9dd7de73bda4d0f251251c5bf8d5e
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-22 16:48:23 +0200

    backend/: Make server use adminRouter

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const homeRouter = require('./routes/home');
 const exerciseRouter = require('./routes/exercise');
+const adminRouter = require('./routes/admin');
 const cors = require('cors');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.static('./frontend/dist'));
 app.use('/api/home', homeRouter);
 app.use('/api/exercise', exerciseRouter);
+app.use('/api/admin', adminRouter);
 
 app
   .listen(port, () => {
