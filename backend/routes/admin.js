9adcc0b625f8ae6f17ea2e63ad1e2d9b31058c3a
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-22 17:54:20 +0200

    backend/: Modify admin signin router to handle sign in

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -7,16 +7,24 @@ adminRouter.post('/signin', async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const validUser = await database.findUser(username);
-    const validPassword = await bcrypt.compare(password, validUser.password);
+    const databaseUser = await database.findUser(username);
 
-    if (validUser && validPassword) {
-      res.send(true);
+    if (databaseUser.length > 0) {
+      const validPassword = await bcrypt.compare(
+        password,
+        databaseUser[0].password,
+      );
+
+      if (validPassword) {
+        res.json({ success: true });
+      } else {
+        res.status(400).json({ success: false });
+      }
     } else {
-      res.send(false);
+      res.status(400).json({ success: false });
     }
   } catch (err) {
-    res.status(err.status).json(err.msg);
+    res.status(500).json({ success: false });
   }
 });
 
@@ -26,12 +34,14 @@ adminRouter.post('/signup', async (req, res) => {
 
     const userExists = await database.findUser(username);
 
-    if (userExists) {
-      res.status(400).json('Username already exists');
+    if (userExists.length > 0) {
+      res.status(400);
+      console.log('cant do sir');
     } else {
       const hashedPassword = await bcrypt.hash(password, 13);
       const newUser = await database.addUser(username, hashedPassword);
       res.json(newUser);
+      console.log('yessir');
     }
   } catch (err) {
     res.status(err.status).json(err.msg);
