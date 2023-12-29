eccd346494edc77e83b8cc91c8fb0fd8ac2117d9
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-29 20:37:34 +0200

    frontend/AddExercise: Add stepper for Add exercise dialog

--- frontend/src/components/AddExercise.jsx
+++ frontend/src/components/AddExercise.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from "@mui/material";
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions,
+        Typography, Stepper, Step, StepLabel } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
 export default function AddExercise() {
     const [open, setOpen] = useState(false);
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = ['Name and category', 'Word pairs', 'Check and save'];
 
     return (
         <>
@@ -12,10 +15,21 @@ export default function AddExercise() {
                 <Typography sx={{ fontWeight: 'bold', pr: 0.5 }}>Add Exercise</Typography>
             </Button>
 
-            <Dialog open={open} onClose={() => setOpen(false)} maxWidth={'md'} fullWidth={true}>
-                <DialogTitle>Add Exercise</DialogTitle>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                maxWidth={'sm'}
+                fullWidth={true}
+                >
+                <DialogTitle sx={{display: 'flex', justifyContent: 'center'}}>Add Exercise</DialogTitle>
                 <DialogContent>
-                    <Typography>Content</Typography>
+                    <Stepper activeStep={activeStep} alternativeLabel>
+                        {steps.map((label) => (
+                            <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={() => setOpen(false)}>Cancel</Button>
