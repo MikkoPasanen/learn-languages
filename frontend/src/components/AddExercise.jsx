8cb05e3a857a4cbdf055de31424b965726b077ab
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-29 22:56:34 +0200

    frontend/AddExercise: Add divider to header

--- frontend/src/components/AddExercise.jsx
+++ frontend/src/components/AddExercise.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Box, Button, Dialog, DialogTitle, DialogContent, DialogActions,
-        Typography, Stepper, Step, StepLabel } from "@mui/material";
+        Typography, Stepper, Step, StepLabel, Divider } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
 export default function AddExercise() {
@@ -25,42 +25,53 @@ export default function AddExercise() {
         }
     }
     return (
-        <>
-            <Button variant="contained" onClick={() => setOpen(true)} sx={{mt: 3}}>
-                <AddIcon sx={{ pr: 0.5, fontSize: '2rem' }} />
-                <Typography sx={{ fontWeight: 'bold', pr: 0.5 }}>Add Exercise</Typography>
-            </Button>
+      <>
+        <Button
+          variant="contained"
+          onClick={() => setOpen(true)}
+          sx={{ mt: 3 }}
+        >
+          <AddIcon sx={{ pr: 0.5, fontSize: '2rem' }} />
+          <Typography sx={{ fontWeight: 'bold', pr: 0.5 }}>
+            Add Exercise
+          </Typography>
+        </Button>
 
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                maxWidth={'sm'}
-                fullWidth={true}
-                sx={{mt: '-30vh'}}
-                >
-                <DialogTitle sx={{display: 'flex', justifyContent: 'center'}}>Add Exercise</DialogTitle>
-                <DialogContent>
-                    <Stepper activeStep={activeStep}>
-                        {steps.map((label) => (
-                            <Step key={label}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        ))}
-                    </Stepper>
-
-                    <Box sx={{mt: 3}}>
-
-                    </Box>
-                </DialogContent>
-                <DialogActions sx={{display: 'flex', justifyContent: 'space-between'}}>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Box>
-                        <Button onClick={handleBack}>Back</Button>
-                        {activeStep < 2 ? <Button onClick={handleNext}>Next</Button> : <Button onClick={handleClose}>Save</Button>}
-
-                    </Box>
-                </DialogActions>
-            </Dialog>
-        </>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          maxWidth={'sm'}
+          fullWidth={true}
+          sx={{ mt: '-30vh' }}
+        >
+          <DialogTitle sx={{ display: 'flex', justifyContent: 'center' }}>
+            Add Exercise
+          </DialogTitle>
+          <DialogContent>
+            <Stepper activeStep={activeStep}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <Divider sx={{mt: 2}}/>
+            <Box sx={{ mt: 3 }}></Box>
+          </DialogContent>
+          <DialogActions
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Button onClick={handleClose}>Cancel</Button>
+            <Box>
+              <Button onClick={handleBack}>Back</Button>
+              {activeStep < 2 ? (
+                <Button onClick={handleNext}>Next</Button>
+              ) : (
+                <Button onClick={handleClose}>Save</Button>
+              )}
+            </Box>
+          </DialogActions>
+        </Dialog>
+      </>
     );
 }
