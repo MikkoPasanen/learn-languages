085c6ae2b91d7b393540880008be0b993e6c0152
Mikko Pasanen <pasanemikko@gmail.com>
2023-12-30 10:43:31 +0200

    frontend/AddExercise: Add input field for language

--- frontend/src/components/AddExercise.jsx
+++ frontend/src/components/AddExercise.jsx
@@ -4,16 +4,19 @@ import { Box, Button, Dialog, DialogTitle, DialogContent, DialogActions,
         Typography, Stepper, Step, StepLabel, Divider, TextField, Autocomplete } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
-export default function AddExercise({ categories }) {
+export default function AddExercise({ categories, languages }) {
     const [open, setOpen] = useState(false);
     const [activeStep, setActiveStep] = useState(0);
+
     const [exerciseName, setExerciseName] = useState('');
     const [category, setCategory] = useState('');
+    const [language, setLanguage] = useState('');
 
     const [nameError, setNameError] = useState(false);
     const [categoryError, setCategoryError] = useState(false);
+    const [languageError, setLanguageError] = useState(false);
 
-    const steps = ['Name and category', 'Word pairs', 'Check and save'];
+    const steps = ['Exercise info', 'Word pairs', 'Check and save'];
 
 
     const handleClose = () => {
@@ -23,14 +26,14 @@ export default function AddExercise({ categories }) {
         setExerciseName('');
         setCategory('');
         setCategoryError(false);
+        setLanguage('');
+        setLanguageError(false);
     }
 
     const handleNext = () => {
         if (activeStep < 2) {
-            if (isValidName() && isValidCategory()) {
+            if (isValidName() && isValidCategory() && isValidLanguage()) {
                 setActiveStep((prevActiveStep) => prevActiveStep + 1);
-                console.log(exerciseName);
-                console.log(category);
             }
         }
     }
@@ -61,6 +64,16 @@ export default function AddExercise({ categories }) {
         }
     }
 
+    const isValidLanguage = () => {
+        if (language === '') {
+            setLanguageError(true);
+            return false
+        } else {
+            setLanguageError(false);
+            return true
+        }
+    }
+
     return (
       <>
         <Button
@@ -85,68 +98,107 @@ export default function AddExercise({ categories }) {
             Add Exercise
           </DialogTitle>
           <DialogContent>
-            <Stepper activeStep={activeStep} alternativeLabel>
+            <Stepper
+              activeStep={activeStep}
+              alternativeLabel
+            >
               {steps.map((label) => (
                 <Step key={label}>
                   <StepLabel>{label}</StepLabel>
                 </Step>
               ))}
             </Stepper>
-            <Divider sx={{mt: 2}}/>
+            <Divider sx={{ mt: 2 }} />
             <Box sx={{ mt: 3 }}>
-                {activeStep === 0 && (
-                    <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-                        <TextField
-                            required
-                            id="exerciseName"
-                            label="Exercise Name"
-                            name="exerciseName"
-                            sx={{width: '80%', mb: 2}}
-                            error={nameError}
-                            helperText={nameError ? 'Name cannot be empty' : ''}
-                            autoFocus
-                            value={exerciseName}
-                            onChange={(e) => setExerciseName(e.target.value)}
-                        />
-                        <Autocomplete
-                            value={category}
-                            onChange={(event, newValue) => {
-                                if(newValue === null) {
-                                    setCategory('');
-                                } else {
-                                    setCategory(newValue);
-                                }
-                            }}
-                            inputValue={category}
-                            onInputChange={(event, newInputValue) => {
-                                setCategory(newInputValue);
-                            }}
-                            freeSolo
-                            options={categories}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Category"
-                                    required
-                                    error={categoryError}
-                                    helperText={categoryError ? 'Category cannot be empty' : ''}
-                                    />
-                            )}
-                            sx={[{width: '80%'}, {mb: 2}]}
-                        >
-                        </Autocomplete>
-                    </Box>
-                )}
-                {activeStep === 1 && (
-                    <>
-                    <Typography>Word pairs</Typography>
-                    </>
-                )}
-                {activeStep === 2 && (
-                    <>
-                    <Typography>Check and save</Typography>
-                    </>
-                )}
+              {activeStep === 0 && (
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <TextField
+                    required
+                    id="exerciseName"
+                    label="Exercise Name"
+                    name="exerciseName"
+                    sx={{ width: '80%', mb: 2 }}
+                    error={nameError}
+                    helperText={nameError ? 'Name cannot be empty' : ''}
+                    autoFocus
+                    value={exerciseName}
+                    onChange={(e) => setExerciseName(e.target.value)}
+                  />
+                  <Autocomplete
+                    value={category}
+                    onChange={(event, newValue) => {
+                      if (newValue === null) {
+                        setCategory('');
+                      } else {
+                        setCategory(newValue);
+                      }
+                    }}
+                    inputValue={category}
+                    onInputChange={(event, newInputValue) => {
+                      setCategory(newInputValue);
+                    }}
+                    freeSolo
+                    options={categories}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Category"
+                        required
+                        error={categoryError}
+                        helperText={
+                          categoryError ? 'Category cannot be empty' : ''
+                        }
+                      />
+                    )}
+                    sx={[{ width: '80%' }, { mb: 2 }]}
+                  ></Autocomplete>
+                  <Autocomplete
+                    value={language}
+                    onChange={(event, newValue) => {
+                      if (newValue === null) {
+                        setLanguage('');
+                      } else {
+                        setLanguage(newValue);
+                      }
+                    }}
+                    inputValue={language}
+                    onInputChange={(event, newInputValue) => {
+                      setLanguage(newInputValue);
+                    }}
+                    freeSolo
+                    options={languages}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label="Language"
+                        required
+                        error={languageError}
+                        helperText={
+                          languageError ? 'Language cannot be empty' : ''
+                        }
+                      />
+                    )}
+                    sx={[{ width: '80%' }, { mb: 2 }]}
+                  ></Autocomplete>
+                </Box>
+              )}
+              {activeStep === 1 && (
+                <>
+                  <Typography>Word pairs</Typography>
+                </>
+              )}
+              {activeStep === 2 && (
+                <>
+                  <Typography>Check and save</Typography>
+                </>
+              )}
             </Box>
           </DialogContent>
           <DialogActions
