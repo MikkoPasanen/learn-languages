2e87888dfb45479ed9708ed4b049aaa53599c747
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-03 00:55:26 +0200

    frontend/data: Add new file that contains all language options

---
+++ frontend/src/data/languages.js
@@ -0,0 +1,514 @@
+const languages = [
+    {
+        name: 'Afrikaans',
+    },
+    {
+        name: 'Albanian',
+    },
+    {
+        name: 'Amharic',
+    },
+    {
+        name: 'Arabic',
+    },
+    {
+        name: 'Armenian',
+    },
+    {
+        name: 'Azerbaijani',
+    },
+    {
+        name: 'Basque',
+    },
+    {
+        name: 'Belarusian',
+    },
+    {
+        name: 'Bengali',
+    },
+    {
+        name: 'Bosnian',
+    },
+    {
+        name: 'Breton',
+    },
+    {
+        name: 'Bulgarian',
+    },
+    {
+        name: 'Burmese',
+    },
+    {
+        name: 'Catalan; Valencian',
+    },
+    {
+        name: 'Chamorro',
+    },
+    {
+        name: 'Chechen',
+    },
+    {
+        name: 'Chichewa',
+    },
+    {
+        name: 'Chinese',
+    },
+    {
+        name: 'Chuvash',
+    },
+    {
+        name: 'Cornish',
+    },
+    {
+        name: 'Corsican',
+    },
+    {
+        name: 'Cree',
+    },
+    {
+        name: 'Croatian',
+    },
+    {
+        name: 'Czech',
+    },
+    {
+        name: 'Danish',
+    },
+    {
+        name: 'Divehi',
+    },
+    {
+        name: 'Dutch',
+    },
+    {
+        name: 'English',
+    },
+    {
+        name: 'Esperanto',
+    },
+    {
+        name: 'Estonian',
+    },
+    {
+        name: 'Ewe',
+    },
+    {
+        name: 'Faroese',
+    },
+    {
+        name: 'Fijian',
+    },
+    {
+        name: 'Finnish',
+    },
+    {
+        name: 'French',
+    },
+    {
+        name: 'Fula',
+    },
+    {
+        name: 'Galician',
+    },
+    {
+        name: 'Georgian',
+    },
+    {
+        name: 'German',
+    },
+    {
+        name: 'Greek',
+    },
+    {
+        name: 'Guaraní',
+    },
+    {
+        name: 'Gujarati',
+    },
+    {
+        name: 'Haitian',
+    },
+    {
+        name: 'Hausa',
+    },
+    {
+        name: 'Hebrew',
+    },
+    {
+        name: 'Herero',
+    },
+    {
+        name: 'Hindi',
+    },
+    {
+        name: 'Hiri Motu',
+    },
+    {
+        name: 'Hungarian',
+    },
+    {
+        name: 'Interlingua',
+    },
+    {
+        name: 'Indonesian',
+    },
+    {
+        name: 'Interlingue',
+    },
+    {
+        name: 'Irish',
+    },
+    {
+        name: 'Igbo',
+    },
+    {
+        name: 'Inupiaq',
+    },
+    {
+        name: 'Ido',
+    },
+    {
+        name: 'Icelandic',
+    },
+    {
+        name: 'Italian',
+    },
+    {
+        name: 'Inuktitut',
+    },
+    {
+        name: 'Japanese',
+    },
+    {
+        name: 'Javanese',
+    },
+    {
+        name: 'Kalaallisut',
+    },
+    {
+        name: 'Kannada',
+    },
+    {
+        name: 'Kanuri',
+    },
+    {
+        name: 'Kashmiri',
+    },
+    {
+        name: 'Kazakh',
+    },
+    {
+        name: 'Khmer',
+    },
+    {
+        name: 'Kikuyu',
+    },
+    {
+        name: 'Kinyarwanda',
+    },
+    {
+        name: 'Kirghiz',
+    },
+    {
+        name: 'Komi',
+    },
+    {
+        name: 'Kongo',
+    },
+    {
+        name: 'Korean',
+    },
+    {
+        name: 'Kurdish',
+    },
+    {
+        name: 'Kwanyama',
+    },
+    {
+        name: 'Latin',
+    },
+    {
+        name: 'Luxembourgish',
+    },
+    {
+        name: 'Luganda',
+    },
+    {
+        name: 'Limburgish',
+    },
+    {
+        name: 'Lingala',
+    },
+    {
+        name: 'Lao',
+    },
+    {
+        name: 'Lithuanian',
+    },
+    {
+        name: 'Luba-Katanga',
+    },
+    {
+        name: 'Latvian',
+    },
+    {
+        name: 'Manx',
+    },
+    {
+        name: 'Macedonian',
+    },
+    {
+        name: 'Malagasy',
+    },
+    {
+        name: 'Malay',
+    },
+    {
+        name: 'Malayalam',
+    },
+    {
+        name: 'Maltese',
+    },
+    {
+        name: 'Māori',
+    },
+    {
+        name: 'Marathi',
+    },
+    {
+        name: 'Marshallese',
+    },
+    {
+        name: 'Mongolian',
+    },
+    {
+        name: 'Nauru',
+    },
+    {
+        name: 'Navajo',
+    },
+    {
+        name: 'Norwegian Bokmål',
+    },
+    {
+        name: 'North Ndebele',
+    },
+    {
+        name: 'Nepali',
+    },
+    {
+        name: 'Ndonga',
+    },
+    {
+        name: 'Norwegian Nynorsk',
+    },
+    {
+        name: 'Norwegian',
+    },
+    {
+        name: 'Nuosu',
+    },
+    {
+        name: 'South Ndebele',
+    },
+    {
+        name: 'Occitan',
+    },
+    {
+        name: 'Ojibwe',
+    },
+    {
+        name: 'Old Church Slavonic',
+    },
+    {
+        name: 'Oromo',
+    },
+    {
+        name: 'Oriya',
+    },
+    {
+        name: 'Ossetian',
+    },
+    {
+        name: 'Panjabi',
+    },
+    {
+        name: 'Pāli',
+    },
+    {
+        name: 'Persian',
+    },
+    {
+        name: 'Polish',
+    },
+    {
+        name: 'Pashto',
+    },
+    {
+        name: 'Portuguese',
+    },
+    {
+        name: 'Quechua',
+    },
+    {
+        name: 'Romansh',
+    },
+    {
+        name: 'Kirundi',
+    },
+    {
+        name: 'Romanian',
+    },
+    {
+        name: 'Russian',
+    },
+    {
+        name: 'Sanskrit',
+    },
+    {
+        name: 'Sardinian',
+    },
+    {
+        name: 'Sindhi',
+    },
+    {
+        name: 'Northern Sami',
+    },
+    {
+        name: 'Samoan',
+    },
+    {
+        name: 'Sango',
+    },
+    {
+        name: 'Serbian',
+    },
+    {
+        name: 'Gaelic',
+    },
+    {
+        name: 'Shona',
+    },
+    {
+        name: 'Sinhala',
+    },
+    {
+        name: 'Slovak',
+    },
+    {
+        name: 'Slovene',
+    },
+    {
+        name: 'Somali',
+    },
+    {
+        name: 'Southern Sotho',
+    },
+    {
+        name: 'Spanish',
+    },
+    {
+        name: 'Sundanese',
+    },
+    {
+        name: 'Swahili',
+    },
+    {
+        name: 'Swati',
+    },
+    {
+        name: 'Swedish',
+    },
+    {
+        name: 'Tamil',
+    },
+    {
+        name: 'Telugu',
+    },
+    {
+        name: 'Tajik',
+    },
+    {
+        name: 'Thai',
+    },
+    {
+        name: 'Tigrinya',
+    },
+    {
+        name: 'Tibetan Standard',
+    },
+    {
+        name: 'Turkmen',
+    },
+    {
+        name: 'Tagalog',
+    },
+    {
+        name: 'Tswana',
+    },
+    {
+        name: 'Tonga',
+    },
+    {
+        name: 'Turkish',
+    },
+    {
+        name: 'Tsonga',
+    },
+    {
+        name: 'Tatar',
+    },
+    {
+        name: 'Twi',
+    },
+    {
+        name: 'Tahitian',
+    },
+    {
+        name: 'Uighur',
+    },
+    {
+        name: 'Ukrainian',
+    },
+    {
+        name: 'Urdu',
+    },
+    {
+        name: 'Uzbek',
+    },
+    {
+        name: 'Venda',
+    },
+    {
+        name: 'Vietnamese',
+    },
+    {
+        name: 'Volapük',
+    },
+    {
+        name: 'Walloon',
+    },
+    {
+        name: 'Welsh',
+    },
+    {
+        name: 'Wolof',
+    },
+    {
+        name: 'Western Frisian',
+    },
+    {
+        name: 'Xhosa',
+    },
+    {
+        name: 'Yiddish',
+    },
+    {
+        name: 'Yoruba',
+    },
+    {
+        name: 'Zhuang',
+    },
+];
+
+export default languages;
