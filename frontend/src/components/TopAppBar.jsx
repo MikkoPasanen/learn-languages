0fc49e69cc04eb8e69a9418bbca280fb7735719a
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-04 17:56:24 +0200

    frontend/TopAppBar: Add media queries so navbar is behind a drawer on mobile screens

--- frontend/src/components/TopAppBar.jsx
+++ frontend/src/components/TopAppBar.jsx
@@ -4,26 +4,150 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import { Drawer, List, ListItemButton, ListItemText } from '@mui/material';
+
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import HomeIcon from '@mui/icons-material/Home';
+import MenuIcon from '@mui/icons-material/Menu';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
 export default function TopAppBar({darkMode, handleThemeChange, signedIn, setSignedIn}) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Toolbar
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            display: { xs: 'none', md: 'block' },
+          }}
+        >
+          <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <Link
+                to="/"
+                style={{
+                  textDecoration: 'none',
+                  color: 'inherit',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <HomeIcon sx={{ pr: 1, fontSize: '2rem' }} />
+                <Typography
+                  variant="h6"
+                  component="div"
+                >
+                  Home
+                </Typography>
+              </Link>
+            </IconButton>
+            <Box sx={{ display: 'flex' }}>
+              {!signedIn && (
+                <Link
+                  to="/signin"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'inherit',
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                  >
+                    <LoginIcon sx={{ pr: 1, fontSize: '2rem' }} />
+                    <Typography
+                      variant="h6"
+                      component="div"
+                    >
+                      Sign In
+                    </Typography>
+                  </IconButton>
+                </Link>
+              )}
+              {signedIn && (
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={() => {
+                    localStorage.removeItem('token');
+                    sessionStorage.removeItem('token');
+                    setSignedIn(false);
+                  }}
+                >
+                  <LogoutIcon sx={{ pr: 1, fontSize: '2rem' }} />
+                  <Typography
+                    variant="h6"
+                    component="div"
+                  >
+                    Sign Out
+                  </Typography>
+                </IconButton>
+              )}
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleThemeChange}
+              >
+                {darkMode ? (
+                  <LightModeIcon sx={{ fontSize: '2rem' }} />
+                ) : (
+                  <DarkModeIcon sx={{ fontSize: '2rem' }} />
+                )}
+              </IconButton>
+            </Box>
+          </Box>
+        </Toolbar>
+        <Toolbar
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + -1,
+            display: { xs: 'block', md: 'none' },
+          }}
+        >
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
+            onClick={() => setDrawerOpen(true)}
           >
+            <MenuIcon sx={{ fontSize: '2.5rem' }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        variant='temporary'
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          '& .MuiDrawer-paper': { width: '30vw' },
+        }}
+      >
+        <List sx={{display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-between'}}>
+          <ListItemButton sx={{flexGrow: 0}}>
             <Link
               to="/"
               style={{
@@ -34,78 +158,47 @@ export default function TopAppBar({darkMode, handleThemeChange, signedIn, setSig
               }}
             >
               <HomeIcon sx={{ pr: 1, fontSize: '2rem' }} />
-              <Typography
-                variant="h6"
-                component="div"
-              >
-                Home
-              </Typography>
+              <ListItemText primary="Home" />
             </Link>
-          </IconButton>
-          <Box sx={{ display: 'flex' }}>
+          </ListItemButton>
+          <Box sx={{}}>
+            <ListItemButton onClick={handleThemeChange}>
+              {darkMode ? (
+                <LightModeIcon sx={{ pr: 1, fontSize: '2rem' }} />
+              ) : (
+                <DarkModeIcon sx={{ pr: 1, fontSize: '2rem' }} />
+              )}
+              <ListItemText primary="Toggle Theme" />
+            </ListItemButton>
             {!signedIn && (
-              <Link
-                to="/signin"
-                style={{
-                  textDecoration: 'none',
-                  color: 'inherit',
-                  display: 'flex',
-                  alignItems: 'center',
-                }}
-              >
-                <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
+              <ListItemButton>
+                <Link
+                  to="/signin"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'inherit',
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
                 >
                   <LoginIcon sx={{ pr: 1, fontSize: '2rem' }} />
-                  <Typography
-                    variant="h6"
-                    component="div"
-                  >
-                    Sign In
-                  </Typography>
-                </IconButton>
-              </Link>
+                  <ListItemText primary="Sign In" />
+                </Link>
+              </ListItemButton>
             )}
             {signedIn && (
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={() => {
-                  localStorage.removeItem('token');
-                  sessionStorage.removeItem('token');
-                  setSignedIn(false);
-                }}
-              >
+              <ListItemButton onClick={() => {
+                localStorage.removeItem('token');
+                sessionStorage.removeItem('token');
+                setSignedIn(false);
+              }}>
                 <LogoutIcon sx={{ pr: 1, fontSize: '2rem' }} />
-                <Typography
-                  variant="h6"
-                  component="div"
-                >
-                  Sign Out
-                </Typography>
-              </IconButton>
+                <ListItemText primary="Sign Out" />
+              </ListItemButton>
             )}
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleThemeChange}
-            >
-              {darkMode ? (
-                <LightModeIcon sx={{ fontSize: '2rem' }} />
-              ) : (
-                <DarkModeIcon sx={{ fontSize: '2rem' }} />
-              )}
-            </IconButton>
           </Box>
-        </Toolbar>
-      </AppBar>
+        </List>
+      </Drawer>
     </Box>
   );
 }
