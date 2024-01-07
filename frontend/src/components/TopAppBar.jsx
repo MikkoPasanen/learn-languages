be8d9ddcd102d68e6f6e2d166ea179586851ab13
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-07 23:21:22 +0200

    frontend/: Reset sign ins

--- frontend/src/components/TopAppBar.jsx
+++ frontend/src/components/TopAppBar.jsx
@@ -38,6 +38,8 @@ export default function TopAppBar({darkMode, handleThemeChange,
     setAnchorEl(null);
   };
 
+  handleSignOut();
+
   // Handle sign out
   const handleSignOut = () => {
     localStorage.removeItem('token');
