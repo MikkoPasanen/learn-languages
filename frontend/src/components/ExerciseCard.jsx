6ed5e831e33c574c19af15d7a79f6ed68dfbbd48
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-08 19:51:02 +0200

    frontend/ExerciseCard: Try out showing user and modify date on exercise card

--- frontend/src/components/ExerciseCard.jsx
+++ frontend/src/components/ExerciseCard.jsx
@@ -40,123 +40,145 @@ export default function ExerciseCard({ exerciseName, exerciseCategory,
     </IconButton>
    ) : null;
 
-   return (
-      <>
-        <Card
-          key={exerciseId}
-          sx={{ mt: 3, borderRadius: 4, width: 380}}
-        >
-          <CardHeader
-            action={action}
-            title={
-              loading ? (
-                <Skeleton
-                  animation="wave"
-                  height={30}
-                  width="80%"
-                  style={{ marginBottom: 6 }}
-                />
-              ) : (
-                  exerciseName
-              )}
-            subheader={
-              loading ? (
-                <Skeleton
-                  animation="wave"
-                  height={20}
-                  width="40%"
-                />
-              ) : (
-              <Box sx={{ mt: 1.5 }}>
-                <Chip
-                  sx={{ mr: 1 }}
-                  label={exerciseCategory}
-                />
-                <Chip label={exerciseLanguage} />
-              </Box>
-            )}
-          ></CardHeader>
-          {signedIn && (
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleOptionsClose}
-            >
-              <MenuItem onClick={handleEditExercise}>
-               <EditExercise
-                  exerciseId={exerciseId}
-                  exerciseName={exerciseName}
-                  exerciseCategory={exerciseCategory}
-                  exerciseLanguage={exerciseLanguage}
-                  categories={categories}
-                  handleReload={handleReload}
-               />
-              </MenuItem>
+  const currentDate = new Date();
+  const formattedDate =
+  `${currentDate.getDate().toString().padStart(2, '0')}/${(currentDate.getMonth() + 1).toString().padStart(2, '0')}/${currentDate.getFullYear().toString()}`;
 
-              <MenuItem onClick={handleOptionsClose}>
-                <DeleteExercise
-                  exerciseId={exerciseId}
-                  exerciseName={exerciseName}
-                  handleReload={handleReload}/>
-              </MenuItem>
-            </Menu>
-          )}
-          <CardContent>
-            {loading ? (
-              <Skeleton
-                animation="wave"
-                variant='rounded'
-                height={40}
-                width={90}
-              />
-            ) : (
-              <Box
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-              >
-                <Button
-                  variant="contained"
-                  sx={{ borderRadius: 2 }}
-                >
-                  <Link
-                    to={`/exercise/${exerciseId}`}
-                    style={{ textDecoration: 'none', color: 'inherit' }}
-                    state={{ exerciseName: exerciseName }}
-                  >
-                    Study🎓
-                  </Link>
-                </Button>
-                <Typography sx={{mr: 1}}>
-                  Status:
-                  {(() => {
+   return (
+     <>
+       <Card
+         key={exerciseId}
+         sx={{ mt: 3, borderRadius: 4, width: 380 }}
+       >
+         <CardHeader
+           action={action}
+           title={
+             loading ? (
+               <Skeleton
+                 animation="wave"
+                 height={30}
+                 width="80%"
+                 style={{ marginBottom: 6 }}
+               />
+             ) : (
+               exerciseName
+             )
+           }
+           subheader={
+             loading ? (
+               <>
+                 <Skeleton
+                   animation="wave"
+                   height={20}
+                   width="40%"
+                 />
+                 <Skeleton
+                   animation="wave"
+                   height={20}
+                   width="40%"
+                 />
+               </>
+             ) : (
+               <>
+                 <Typography>
+                   Made by: <strong>{localStorage.getItem('username')}</strong>
+                 </Typography>
+                  <Typography>
+                    <small>Last modified: {formattedDate}</small>
+                  </Typography>
+                 <Box sx={{ mt: 1.5 }}>
+                   <Chip
+                     sx={{ mr: 1 }}
+                     label={exerciseCategory}
+                   />
+                   <Chip label={exerciseLanguage} />
+                 </Box>
+               </>
+             )
+           }
+         ></CardHeader>
+         {signedIn && (
+           <Menu
+             anchorEl={anchorEl}
+             open={Boolean(anchorEl)}
+             onClose={handleOptionsClose}
+           >
+             <MenuItem onClick={handleEditExercise}>
+               <EditExercise
+                 exerciseId={exerciseId}
+                 exerciseName={exerciseName}
+                 exerciseCategory={exerciseCategory}
+                 exerciseLanguage={exerciseLanguage}
+                 categories={categories}
+                 handleReload={handleReload}
+               />
+             </MenuItem>
+
+             <MenuItem onClick={handleOptionsClose}>
+               <DeleteExercise
+                 exerciseId={exerciseId}
+                 exerciseName={exerciseName}
+                 handleReload={handleReload}
+               />
+             </MenuItem>
+           </Menu>
+         )}
+         <CardContent>
+           {loading ? (
+             <Skeleton
+               animation="wave"
+               variant="rounded"
+               height={40}
+               width={90}
+             />
+           ) : (
+             <Box
+               sx={{
+                 display: 'flex',
+                 justifyContent: 'space-between',
+                 alignItems: 'center',
+               }}
+             >
+               <Button
+                 variant="contained"
+                 sx={{ borderRadius: 2 }}
+               >
+                 <Link
+                   to={`/exercise/${exerciseId}`}
+                   style={{ textDecoration: 'none', color: 'inherit' }}
+                   state={{ exerciseName: exerciseName }}
+                 >
+                   Study🎓
+                 </Link>
+               </Button>
+               <Typography sx={{ mr: 1 }}>
+                 Status:
+                 {(() => {
                    if (userScore === null) {
-                    return (
-                      <Typography sx={{ fontWeight: 'bold', color: 'red' }}>
-                        Not started❌
-                      </Typography>
-                    );
+                     return (
+                       <Typography sx={{ fontWeight: 'bold', color: 'red' }}>
+                         Not started❌
+                       </Typography>
+                     );
                    } else if (userScore === totalScore) {
-                    return (
-                      <Typography sx={{ fontWeight: 'bold', color: 'green' }}>
-                        Completed🎉
-                      </Typography>
-                    );
+                     return (
+                       <Typography sx={{ fontWeight: 'bold', color: 'green' }}>
+                         Completed🎉
+                       </Typography>
+                     );
                    } else {
-                    return (
-                      <Typography sx={{ fontWeight: 'bold', color: 'orange' }}>
-                        In progress⌛
-                      </Typography>
-                    );
+                     return (
+                       <Typography sx={{ fontWeight: 'bold', color: 'orange' }}>
+                         In progress⌛
+                       </Typography>
+                     );
                    }
-                  })()}
-                </Typography>
-              </Box>
-            )}
-          </CardContent>
-        </Card>
-      </>
-    );
+                 })()}
+               </Typography>
+             </Box>
+           )}
+         </CardContent>
+       </Card>
+     </>
+   );
 }
