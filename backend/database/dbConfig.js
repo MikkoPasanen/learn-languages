083cb9d98171a214dcd00a285fd47c612e683b1f
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-10 03:13:11 +0200

    backend/database/dbConfig: Add documentation to the file

--- backend/database/dbConfig.js
+++ backend/database/dbConfig.js
@@ -1,7 +1,25 @@
+/**
+ * @fileOverview Database configuration file
+ * @module backend/database/dbConfig
+ * @requires dotenv
+ * @requires mysql
+ */
+
+// Load environment variables from .env file
 require('dotenv').config();
+
+// Load mysql
 const mysql = require('mysql');
 
-// mysql configuration
+/**
+ * @description: MySQL configuration object.
+ * @type {Object}
+ * @property {string} host - MySQL server host.
+ * @property {string} user - MySQL user.
+ * @property {string} password -  Password of the MySQL user.
+ * @property {string} database - The MySQL database to connect to.
+ * @property {number} connectionLimit - The maximum number of connections to the MySQL server.
+ */
 const config = {
   host: process.env.HOST,
   user: process.env.USER,
@@ -10,6 +28,11 @@ const config = {
   connectionLimit: process.env.LIMIT,
 };
 
+/**
+ * @description: Create a MySQL connection pool.
+ * @type {mysql.Pool}
+ */
 const pool = mysql.createPool(config);
 
+// Export the pool
 module.exports = pool;
