f24b63497c550eca2714b258012559ddbf3f0cae
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-11 00:24:44 +0200

    frontend/App: Add documentation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,12 @@
+/**
+ * Entry point for the frontend.
+ * @module frontend/src/main
+ * @requires react
+ * @requires react-dom/client
+ * @requires App
+ * @author Mikko Pasanen
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
