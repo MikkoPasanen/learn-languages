402cf6438297171444b387cd7c1f0c6d6c3ac37a
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-10 03:12:46 +0200

    backend/server: Add documentation to the file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+/**
+ * @description: This file is the entry point for the backend server.
+ */
 const express = require('express');
 const homeRouter = require('./routes/home');
 const exerciseRouter = require('./routes/exercise');
@@ -5,10 +8,19 @@ const credentialsRouter = require('./routes/credentials');
 const adminRouter = require('./routes/admin');
 const cors = require('cors');
 
+/**
+ * Create Express server.
+ * @type {Express}
+ */
 const app = express();
 const port = 8080;
 
-// Enable CORS so backend and frontend can communicate
+/**
+ * Express configuration.
+ * Enable CORS and JSON parsing.
+ * Serve the frontend from the backend.
+ * Set up the routes.
+ */
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(express.static('./frontend/dist'));
@@ -17,15 +29,26 @@ app.use('/api/exercise', exerciseRouter);
 app.use('/api/credentials', credentialsRouter);
 app.use('/api/admin', adminRouter);
 
-// If no route is matched, return the frontend
+/**
+ * If no API route is matched, send the React app which will display error page.
+ */
 app.all('*', (req, res) => {
   res.sendFile('index.html', { root: './frontend/dist' });
 });
 
+/**
+ * Start the server.
+ */
 app
   .listen(port, () => {
+    /**
+     * This callback is called when the server starts succesfully.
+     */
     console.log(`SERVER: listening on port ${port}`);
   })
   .on('error', (err) => {
+    /**
+     * This callback is called if the server fails to start.
+     */
     console.log(`SERVER: Error starting the server ${err}`);
   });
