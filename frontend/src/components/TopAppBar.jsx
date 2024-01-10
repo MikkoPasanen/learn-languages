739714840584c57f862f12a797496e96edbe0e88
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-11 00:25:01 +0200

    frontend/TopAppBar: Add documentation

--- frontend/src/components/TopAppBar.jsx
+++ frontend/src/components/TopAppBar.jsx
@@ -1,4 +1,13 @@
 /* eslint-disable react/prop-types */
+
+/**
+ * @fileoverview Top app bar component. This component is displayed at the top of the page.
+ * @component
+ * @requires NPM:react-router-dom
+ * @requires NPM:@mui/material
+ * @requires NPM:@mui/icons-material
+ * @requires NPM:react
+ */
 import { Drawer, List, ListItemButton, ListItemText, ListItem,
          Divider, Accordion, AccordionSummary, AccordionDetails,
          Checkbox, Chip, Badge, Avatar, IconButton, Typography, Box,
@@ -17,29 +26,61 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import TuneIcon from '@mui/icons-material/Tune';
 
+/**
+ * Top app bar component - displayed at the top of the page
+ * @description Contains the navigation links and the filters
+ * and displays the user's profile picture.
+ * @param {Object} props Component props
+ * @param {boolean} props.darkMode Whether the dark mode is enabled
+ * @param {function} props.handleThemeChange Function to handle the theme change
+ * @param {boolean} props.signedIn Whether the user is signed in
+ * @param {function} props.setSignedIn Function to set the signed in state
+ * @param {function} props.setOpenAddExercise Function to set the open add exercise dialog state
+ * @param {string[]} props.categories Array of categories
+ * @param {string[]} props.languages Array of languages
+ * @param {string[]} props.madeBy Array of creators of the exercises
+ * @param {Object[]} props.exercises Array of exercises
+ * @param {function} props.setMobileFilteredExercises Function to set the filtered exercises
+ * @returns {JSX.Element} Top app bar component
+*/
 export default function TopAppBar({darkMode, handleThemeChange,
                                   signedIn, setSignedIn,
                                   setOpenAddExercise, categories, languages,
                                   madeBy, exercises, setMobileFilteredExercises,}) {
+
+  // State for the drawer on mobile
   const [drawerOpen, setDrawerOpen] = useState(false);
+  // State for the number of filters
   const [filterCount, setFilterCount] = useState(0);
+
+  // State for the filters
   const [filterCategories, setFilterCategories] = useState([]);
   const [filterLanguages, setFilterLanguages] = useState([]);
   const [filterMadeBy, setFilterMadeBy] = useState([]);
   const [filterStatuses, setFilterStatuses] = useState([]);
+
+  // State for the profile menu
   const [anchorEl, setAnchorEl] = useState(null);
 
-  // Handle the profile menu
+  /**
+   * Handle the profile menu
+   * @param {Object} e Event object which triggered the function
+   */
   const handleProfileClick = (e) => {
     setAnchorEl(e.currentTarget);
   };
 
-  // Handle the profile menu
+  /**
+   * Handle the profile menu close
+   * @param {Object} e Event object which triggered the function
+   */
   const handleProfileClose = () => {
     setAnchorEl(null);
   };
 
-  // Handle sign out
+  /**
+   * Handle the sign out process
+   */
   const handleSignOut = () => {
     localStorage.removeItem('token');
     sessionStorage.removeItem('token');
@@ -49,87 +90,130 @@ export default function TopAppBar({darkMode, handleThemeChange,
     setAnchorEl(null);
   };
 
-  // Store the number of exercises in each category
+  /**
+   * Store the number of exercises in each category
+   * @type {Object[]}
+   * @property {string} category Category name
+   * @property {number} count Number of exercises in the category
+   */
   const categoryCounts = categories.map((category) => {
     const count = exercises.filter((exercise) => exercise.category === category).length;
     return { category, count };
   });
 
-  // Store the number of exercises in each language
+  /**
+   * Store the number of exercises in each language
+   * @type {Object[]}
+   * @property {string} language Language name
+   * @property {number} count Number of exercises in the language
+   */
   const languageCounts = languages.map((language) => {
     const count = exercises.filter((exercise) => exercise.language === language).length;
     return { language, count };
   });
 
-  // Store the number of exercises made by each user
+  /**
+   * Store the number of exercises made by each user
+   * @type {Object[]}
+   * @property {string} user User name
+   * @property {number} count Number of exercises made by the user
+   */
   const madeByCounts = madeBy.map((user) => {
     const count = exercises.filter((exercise) => exercise.made_by === user).length;
     return { user, count };
   });
 
-  // Filter the exercises based on the selected categories
-    const categoryChange = (e) => {
-      if (e.target.checked) {
-        setFilterCategories([...filterCategories, e.target.name]);
-      } else {
-        setFilterCategories(filterCategories.filter((category) => category !== e.target.name));
-      }
-    };
+ /**
+  * Filter the exercises based on the selected categories
+  * @param {Object} e Event object which triggered the function
+  * @param {string} e.target.name Category name
+  * @param {boolean} e.target.checked Whether the category is checked
+  */
+  const categoryChange = (e) => {
+    if (e.target.checked) {
+      setFilterCategories([...filterCategories, e.target.name]);
+    } else {
+      setFilterCategories(filterCategories.filter((category) => category !== e.target.name));
+    }
+  };
 
-    // Filter the exercises based on the selected languages
-    const languageChange = (e) => {
-      if (e.target.checked) {
-        setFilterLanguages([...filterLanguages, e.target.name]);
-      } else {
-        setFilterLanguages(filterLanguages.filter((language) => language !== e.target.name));
-      }
-    };
+  /**
+   * Filter the exercises based on the selected languages
+   * @param {Object} e Event object which triggered the function
+   * @param {string} e.target.name Language name
+   * @param {boolean} e.target.checked Whether the language is checked
+  */
+  const languageChange = (e) => {
+    if (e.target.checked) {
+      setFilterLanguages([...filterLanguages, e.target.name]);
+    } else {
+      setFilterLanguages(filterLanguages.filter((language) => language !== e.target.name));
+    }
+  };
 
-    // Filter the exercises based on the selected statuses
-    const statusChange = (e) => {
-      const { name, checked } = e.target;
-      if (checked) {
-        setFilterStatuses((prev) => [...prev, name]);
-      } else {
-        setFilterStatuses((prev) => prev.filter((status) => status !== name));
-      }
-    };
+  /**
+   * Filter the exercises based on the selected statuses
+   * @param {Object} e Event object which triggered the function
+   * @param {string} e.target.name Status name
+   * @param {boolean} e.target.checked Whether the status is checked
+   */
+  const statusChange = (e) => {
+    const { name, checked } = e.target;
+    if (checked) {
+      setFilterStatuses((prev) => [...prev, name]);
+    } else {
+      setFilterStatuses((prev) => prev.filter((status) => status !== name));
+    }
+  };
 
-    // Filter the exercises based on the selected made by
-    const madeByChange = (e) => {
-      if (e.target.checked) {
-        setFilterMadeBy([...filterMadeBy, e.target.name]);
-      } else {
-        setFilterMadeBy(filterMadeBy.filter((madeBy) => madeBy !== e.target.name));
-      }
-    };
+  /**
+   * Filter the exercises based on the selected creators
+   * @param {Object} e Event object which triggered the function
+   * @param {string} e.target.name Creator name
+   * @param {boolean} e.target.checked Whether the creator is checked
+   */
+  const madeByChange = (e) => {
+    if (e.target.checked) {
+      setFilterMadeBy([...filterMadeBy, e.target.name]);
+    } else {
+      setFilterMadeBy(filterMadeBy.filter((madeBy) => madeBy !== e.target.name));
+    }
+  };
 
-    useEffect(() => {
-      // Filter the exercises based on the selected filters
-      const filteredExercises = exercises.filter((exercise) => {
-        const score = localStorage.getItem(`${exercise.id}-userScore`);
-        const totalScore = localStorage.getItem(`${exercise.id}-totalScore`);
-        const status =
-          score === null
-            ? 'Not started'
-            : score === totalScore
-              ? 'Completed'
-              : 'In progress';
+  /**
+   * When component mounts, set the exercises based on the selected filters
+   */
+  useEffect(() => {
+    // Filter the exercises based on the selected filters
+    const filteredExercises = exercises.filter((exercise) => {
+      const score = localStorage.getItem(`${exercise.id}-userScore`);
+      const totalScore = localStorage.getItem(`${exercise.id}-totalScore`);
+      // Calculate the status of the exercise based on the score
+      const status =
+        score === null
+          ? 'Not started'
+          : score === totalScore
+            ? 'Completed'
+            : 'In progress';
 
-        return (
-          (filterCategories.length === 0 ||
-            filterCategories.includes(exercise.category)) &&
-          (filterLanguages.length === 0 ||
-            filterLanguages.includes(exercise.language)) &&
-          (filterStatuses.length === 0 || filterStatuses.includes(status)) &&
-          (filterMadeBy.length === 0 || filterMadeBy.includes(exercise.made_by))
-        );
-      });
+      // Check if there are no filters or if the exercise matches the filters
+      return (
+        (filterCategories.length === 0 ||
+          filterCategories.includes(exercise.category)) &&
+        (filterLanguages.length === 0 ||
+          filterLanguages.includes(exercise.language)) &&
+        (filterStatuses.length === 0 || filterStatuses.includes(status)) &&
+        (filterMadeBy.length === 0 || filterMadeBy.includes(exercise.made_by))
+      );
+    });
+    setMobileFilteredExercises(filteredExercises);
+  }, [exercises, setMobileFilteredExercises, filterCategories, filterLanguages, filterStatuses, filterMadeBy]);
 
-      setMobileFilteredExercises(filteredExercises);
-    }, [exercises, setMobileFilteredExercises, filterCategories, filterLanguages, filterStatuses, filterMadeBy]);
-
-
+  /**
+   * Render the component
+   * Different components are rendered based on the screen size
+   * @returns {JSX.Element} Top app bar component
+   */
   return (
     <Box>
       <AppBar position="fixed">
