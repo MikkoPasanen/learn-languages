a52fed85b80689ff8e8252ae2e816feb568cbad7
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-10 03:13:55 +0200

    backend/routes/exercise: Add documentation to the file

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -1,16 +1,33 @@
+/**
+ * @fileoverview This file handles all requests to the /exercise endpoint.
+ * @description Provides route for fetching exercise data from the database.
+ * @module routes/exercise
+ */
+
 const express = require('express');
 const database = require('../database/methods');
+
+// Create a router
 const exerciseRouter = express.Router();
 
-// Handles requests for fetching all results from a db table
+/**
+ * Route handler for fetching exercise data.
+ * @async
+ * @param {express.Request} req - The Express request object.
+ * @param {express.Response} res - The Express response object.
+ */
 exerciseRouter.get('/:id([0-9]+)', async (req, res) => {
   try {
+    // Get the exercise data from the database
     const results = await database.fetchExercise(req.params.id);
 
+    // Send the exercise data to the client
     res.json(results);
   } catch (err) {
+    // Send an error response if something went wrong
     res.status(err.status).json(err.msg);
   }
 });
 
+// Export the router
 module.exports = exerciseRouter;
