474b48ce964f9e4b1eda5652dceedf25860d9d36
Mikko Pasanen <pasanemikko@gmail.com>
2024-01-11 00:23:04 +0200

    frontend/App: Add documentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,13 @@
+/**
+ * @fileoverview App component is the main component of the application.
+ * @requires react
+ * @requires react-router-dom
+ * @requires @mui/material
+ * @requires @mui/material/styles
+ * @requires Home
+ *
+ */
+
 import { useState, useEffect } from "react";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
@@ -10,49 +20,83 @@ import TopAppBar from"./components/TopAppBar.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 
+
+/**
+ * App component - renders the main component of the application
+ * @returns {JSX.Element} - Rendered component
+*/
 export default function App() {
+  // State for the theme
   const [darkMode, setDarkMode] = useState(true);
+  // State for the sign in status
   const [signedIn, setSignedIn] = useState(false);
+  // State for the add exercise dialog
   const [openAddExercise, setOpenAddExercise] = useState(false);
+  // State for the exercises
   const [exercises, setExercises] = useState([]);
+  // State for the loading spinner
   const [loading, setLoading] = useState(true);
+  // State for the unique categories
   const [categories, setCategories] = useState([]);
+  // State for the unique languages
   const [languages, setLanguages] = useState([]);
+  // State for the creators of the exercises
   const [madeBy, setMadeBy] = useState([]);
+  // State for exercises filtered by mobile
   const [mobileFilteredExercises, setMobileFilteredExercises] = useState([]);
 
-
+  // Create a theme with the dark or light mode
   const theme = createTheme({
     palette: {
-      mode: darkMode ? "dark" : "light",
+      mode: darkMode ? 'dark' : 'light',
     },
   });
 
+  /**
+   * Effect hook for setting the signed in state and fetching exercises.
+   * Runs once on component mount.
+   */
   useEffect(() => {
-    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+    // Get the token from local storage or session storage
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+    // If the token exists, set the signed in state to true
     if (token) {
       setSignedIn(true);
     }
+    // Fetch the exercises
     fetchExercises();
-  }, [])
+  }, []);
 
+  /**
+   * Handles the reloading of the exercises by fetching them again
+   * @async
+  */
   const handleReload = async () => {
     fetchExercises();
   };
 
+  /**
+   * Fetches the exercises from the backend and sets the states
+   * @async
+   */
   const fetchExercises = async () => {
+    // Send GET request to backend
     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/home`);
+    // Get the response as JSON
     const data = await response.json();
     setExercises(data);
+
+    // Get the categories, languages and creators
     const categories = data.map((exercise) => exercise.category);
     const languages = data.map((exercise) => exercise.language);
     const madeBy = data.map((exercise) => exercise.made_by);
+
+    // Set the states with the unique values
     setCategories([...new Set(categories)]);
     setLanguages([...new Set(languages)]);
     setMadeBy([...new Set(madeBy)]);
     setLoading(false);
   };
-
 
   return (
     <>
@@ -70,7 +114,6 @@ export default function App() {
             madeBy={madeBy}
             exercises={exercises}
             setMobileFilteredExercises={setMobileFilteredExercises}
-
           />
           <Routes>
             <Route
